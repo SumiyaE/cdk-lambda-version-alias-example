89a3caa117faa247c0cdc8de5567ceff01f7a213
江口純矢 <s_eguchi@codmon.co.jp>
2024-09-08 16:20:58 +0900

    lambdaのコードのhash値が毎回変わるため、それらを無視するためのserializerを追加

--- test/cdk-lambda-version-alias-example.test.ts
+++ test/cdk-lambda-version-alias-example.test.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as CdkLambdaVersionAliasExample from '../lib/cdk-lambda-version-alias-example-stack';
 import {expect, test} from "vitest";
+import {ignoreAssetHashSerializer} from "./plugins/ignore-asset-hash";
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cdk-lambda-version-alias-example-stack.ts
 test('SQS Queue Created', () => {
@@ -10,5 +11,7 @@ test('SQS Queue Created', () => {
   const template = Template.fromStack(stack);
 
   // snapshot testを実行する
+  // s3のbucket名やlambdaのコードのhash値が毎回変わるため、それらを無視するためのserializerを追加
+  expect.addSnapshotSerializer(ignoreAssetHashSerializer);
   expect(template).toMatchSnapshot();
 });
