e8d0bfdabaf47fc06fa9eadfab99c3c281277600
江口純矢 <s_eguchi@codmon.co.jp>
2024-09-08 16:22:38 +0900

    handler.tsの処理を簡素化

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,10 +1,9 @@
 import {Context, SQSEvent} from "aws-lambda";
 
 export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
-  console.log(`Event: ${JSON.stringify(event, null, 2)}`)
-  console.log(`Context: ${JSON.stringify(context, null, 2)}`)
-
+  console.log('-------------ここから処理開始-------------')
   for (const record of event.Records) {
     console.log(`Message body: ${record.body}`)
   }
+  console.log('-------------ここで処理終了-------------')
 }
