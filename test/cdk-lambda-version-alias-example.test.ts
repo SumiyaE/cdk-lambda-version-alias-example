be91100520a6123ccb9cb0af7c4c4678ba7c72d7
江口純矢 <s_eguchi@codmon.co.jp>
2024-09-08 16:23:31 +0900

    不要なコメントを削除

--- test/cdk-lambda-version-alias-example.test.ts
+++ test/cdk-lambda-version-alias-example.test.ts
@@ -3,8 +3,6 @@ import { Template } from 'aws-cdk-lib/assertions';
 import * as CdkLambdaVersionAliasExample from '../lib/cdk-lambda-version-alias-example-stack';
 import {expect, test} from "vitest";
 import {ignoreAssetHashSerializer} from "./plugins/ignore-asset-hash";
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-lambda-version-alias-example-stack.ts
 test('SQS Queue Created', () => {
   const app = new cdk.App();
   const stack = new CdkLambdaVersionAliasExample.CdkLambdaVersionAliasExampleStack(app, 'MyTestStack');
