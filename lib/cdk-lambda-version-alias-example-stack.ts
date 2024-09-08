acf5ca67646366099aaa52afc0ab6b9148ff2fc2
江口純矢 <s_eguchi@codmon.co.jp>
2024-09-08 14:13:17 +0900

    SQSをイベントソースとするlambdaの作成

--- lib/cdk-lambda-version-alias-example-stack.ts
+++ lib/cdk-lambda-version-alias-example-stack.ts
@@ -1,13 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
 
 export class CdkLambdaVersionAliasExampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // SQSの作成
     const queue = new sqs.Queue(this, 'CdkLambdaVersionAliasExampleQueue', {
       visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    // lambdaの作成
+    const lambda = new NodejsFunction(this, 'CdkLambdaVersionAliasExampleFunction', {
+      runtime: Runtime.NODEJS_LATEST,
+      entry: 'lambda/handler.ts',
+      handler: 'handler',
     });
   }
 }
